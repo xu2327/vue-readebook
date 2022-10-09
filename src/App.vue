bf86fb994154294406c632e9bc95d4ca3276014c
xu2327 <2327801183@qq.com>
2022-10-09 16:16:23 +0800

    feat: 第三章

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <span class="text">ABCDEFG</span>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['test'])
+  },
+  mounted () {
+    this.$store.dispatch('setTest', 50).then(() => {
+      console.log(this.test)
+    })
+  }
+}
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+  })
+</script>
+
+<style lang="scss" scoped>
+@import "./assets/styles/global";
+.text {
+  font-family: 'Days One';
+  font-size:px2rem(20);
+  color: orange;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <span class="icon-book2"></span>
+    <span class="icon-clock"></span>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+  .icon-book2 {
+    font-size: 20px;
+    color: red;
+  }
+</style>
