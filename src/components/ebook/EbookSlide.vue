660453c3de2c2fa561403cf5599d592475e9f7fb
xu2327 <2327801183@qq.com>
2022-10-14 03:13:39 +0800

    feat: 目录加载动画效果

--- src/components/ebook/EbookSlide.vue
+++ src/components/ebook/EbookSlide.vue
@@ -3,7 +3,7 @@
         <div class="slide-content-wrapper" v-show="menuVisible && settingVisible === 3">
             <transition name="slide-right">
                 <div class="content" v-if="settingVisible === 3">
-                    <div class="content-page-wrapper">
+                    <div class="content-page-wrapper" v-if="bookAvailable">
                         <div class="content-page">
                             <component :is="currentTab === 1 ? content : bookmark"></component>
                         </div>
@@ -20,6 +20,9 @@
                             </div>
                         </div>
                     </div>
+                    <div class="content-empty" v-else>
+                        <ebook-loading></ebook-loading>
+                    </div>
                 </div>
             </transition>
             <div class="content-bg" @click="hideTitleAndMenu()"></div>
@@ -30,8 +33,12 @@
 <script>
 import { ebookMixin } from '@/utils/mixin'
 import EbookSlideContents from './EbookSlideContent'
+import EbookLoading from './EbookLoading'
 export default {
     mixins: [ebookMixin],
+    components: {
+        EbookLoading
+    },
     data () {
         return {
             currentTab: 1,
@@ -82,6 +89,11 @@ export default {
                         @include center;
                     }
                 }
+            }
+            .content-empty {
+                width: 100%;
+                height: 100%;
+                @include center;
             }
         }
         .content-bg {
