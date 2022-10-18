bed3450bb5506df6790a749e86b44b3bb99839fd
xu2327 <2327801183@qq.com>
2022-10-18 17:24:20 +0800

    feat: 搜索框实现

---
+++ src/views/store/StoreShelf.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="store-shelf">
+    <shelf-title></shelf-title>
+    <scroll class="store-shelf-scroll-wrapper" :top="0">
+      <shelf-search></shelf-search>
+    </scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from '../../components/common/Scroll.vue'
+import ShelfSearch from '../../components/shelf/ShelfSearch.vue'
+import ShelfTitle from '../../components/shelf/ShelfTitle.vue'
+import { storeShelfMixin } from '../../utils/mixin'
+
+export default {
+  mixins: [ storeShelfMixin ],
+  components: {
+    Scroll,
+    ShelfTitle,
+    ShelfSearch
+  }
+
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+    @import "../../assets/styles/global";
+
+    .store-shelf {
+      position: relative;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      background: white;
+      .store-shelf-scroll-wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 101;
+      }
+    }
+</style>
